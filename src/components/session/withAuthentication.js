dcb757fc07c5e42fd5c49eba68dceb9d3ccb9f27
Kyle Gill <kylerobertgill@gmail.com>
2019-03-27 15:19:50 -0600

    feat: session handling

---
+++ src/components/session/withAuthentication.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import AuthUserContext from "./context";
+import { withFirebase } from "../firebase";
+
+const withAuthentication = Component => {
+  class WithAuthentication extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        authUser: JSON.parse(localStorage.getItem("authUser"))
+      };
+    }
+
+    componentDidMount() {
+      this.listener = this.props.firebase.auth.onAuthStateChanged(
+        authUser => {
+          localStorage.setItem("authUser", JSON.stringify(authUser));
+          this.setState({ authUser });
+        },
+        () => {
+          localStorage.removeItem("authUser");
+          this.setState({ authUser: null });
+        }
+      );
+    }
+
+    componentWillUnmount() {
+      this.listener();
+    }
+
+    render() {
+      return (
+        <AuthUserContext.Provider value={this.state.authUser}>
+          <Component authUser={this.state.authUser} {...this.props} />
+        </AuthUserContext.Provider>
+      );
+    }
+  }
+
+  return withFirebase(WithAuthentication);
+};
+
+export default withAuthentication;
