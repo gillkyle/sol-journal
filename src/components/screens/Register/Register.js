daeaa9649d521fb45c4e7b42486c6571195918af
Kyle Gill <kylerobertgill@gmail.com>
2019-04-21 20:37:28 -0600

    chore: autofocus text fields

--- src/components/screens/Register/Register.js
+++ src/components/screens/Register/Register.js
@@ -67,7 +67,6 @@ class RegisterFormBase extends Component {
           .doc(user.uid)
           .set({
             email: user.email,
-            theme: "LIGHT",
           })
         this.props.history.push("/")
       })
